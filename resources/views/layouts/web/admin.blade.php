5c8a4080870ea5e88222d181db07625baacd6409
Danyellita <daniella.stresna@gmail.com>
2021-01-14 21:06:24 +0200

    products page review

--- resources/views/layouts/web/admin.blade.php
+++ resources/views/layouts/web/admin.blade.php
@@ -3,6 +3,7 @@
 <meta charset="utf-8">
 <title>Farmacie Online</title>
 <head>
+  <meta name="viewport" content="width=device-width, initial-scale=1">
   <link href="{{ asset('css/app.css') }}" rel="stylesheet">
   <link href="{{ asset('css/newstyle.css') }}" rel="stylesheet">
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
--- resources/views/web/categories/index.blade.php
+++ resources/views/web/categories/index.blade.php
@@ -10,20 +10,28 @@
 				      		<div class="card-header" id="myTable">
 				      			<h5 style="color: black;"> <a href="#" style="color:black; text-decoration: none;">Category<br>{{ $category->name }} </a></h5>
 				      		</div>
-				      		<div class="card-body"> 
-								<h5>Products</h5>
-					       		<div class="row">
-					       			@foreach($category->products as $product)
-					       				<div class="col">
-					       					<a href="{{ route('web.products.show', [$product]) }}">{{ $product->name}}</a>
-					       					@if($product->picture)
-						  						<img src="{{ asset('storage/img')}}/{{ $product->picture }}" style="width: 100px; height: 100px; padding: 10px 10px 10px 10px">
-						  					@else
-						  						<img src="{{ asset('img/4.jpg')}}" style="width: 100px; height: 100px; padding: 10px 10px 10px 10px">
-						  					@endif 
-					       				</div>
-						  			@endforeach
-					       		</div>
+				      		<div class="card-body">
+					      		<ul class="cards">
+									@foreach($category->products as $product)
+										<li class="cards_item">
+											<div class="card">
+												<center><h4 class="card_title" style="color: black; padding-top: 30px;"><a href="{{ route('web.products.show', [$product]) }}" style="color: black;">{{ $product->name}}</h4></center>
+										      	<div class="card_image">
+										      		</a>
+										      		@if($product->picture)
+							  						<img src="{{ asset('storage/img')}}/{{ $product->picture }}" style="width: 100px; height: 100px; padding: 10px 10px 10px 10px">
+							  					@else
+							  						<img src="{{ asset('img/4.jpg')}}" style="width: 100px; height: 100px; padding: 10px 10px 10px 10px">
+							  					@endif 
+								  				</div>
+										        <div class="card_content">
+										          	<p class="card_text">Lorem ipsum dolor sit amet, consectetur adipiscing elit <br>sed do eiusmod...<span style="color:green">Details</span></p>
+										          	
+										        </div>
+										    </div>
+										</li>
+									@endforeach
+								</ul> 
 							</div>
 				        </div>
 					</div>
